ce90ab99cbab84e332c9b51ac5bb0722b305a2a3
Pieter van der Eems <p.van.der.eems@interactiveblueprints.nl>
2015-02-25 14:45:37 +0100

    Blog.js added, start replications, get characters

---
+++ blog.js
@@ -0,0 +1,59 @@
+/*global PouchDB*/
+/*jslint browser:true*/
+(function () {
+    'use strict';
+    var db = new PouchDB('zone'),
+        characters = {},
+        refreshContent;
+
+    refreshContent = function (character) {
+        db.get(characters[character].docId, function (err, doc) {
+            if (err) {
+                console.error('Error getting character doc', {character: character, docId: characters[character].docId, error: err});
+                return;
+            }
+            characters[character].node.innerHTML = doc.blog;
+        });
+    };
+
+    // ** Determine characters **
+
+    document.querySelectorAll('[data-character]').forEach(function (elm) {
+        characters[elm.dataset.character] = {
+            node: elm
+        };
+        db.query({
+            map: function (doc, emit) {
+                emit(doc.name);
+            }
+        }, {
+            reduce: false
+        }, {
+            key: elm.dataset.character
+        }, function (err, response) {
+            if (err) {
+                console.error('Error querying database for character', elm.dataset.character, err);
+                return;
+            }
+            console.log(response);
+//            characters[elm.dataset.character].docId = result
+//            refreshContent()
+        });
+    });
+
+
+    // *
+    // ** Database
+    // *
+
+
+    // ** Start replications **
+    db.replicate.from('https://zone.mekton.nl/db/zone', {live: true, filter: 'zone/characters'})
+        .on('error', function (err) {
+            console.error('Error replicating from zone', err);
+        });
+    db.replicate.to('https://zone.mekton.nl/db/zone', {live: true})
+        .on('error', function (err) {
+            console.error('Error replicating to zone', err);
+        });
+}());
