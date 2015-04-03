fcc169b79fe4d8ddfc766ff801e21e11256b1c47
Pieter van der Eems <pieter@eemco.nl>
2015-04-03 22:25:59 +0200

    talk, use vague time

--- talk/js/talk.js
+++ talk/js/talk.js
@@ -1,6 +1,6 @@
 /*global define*/
 /*jslint browser:true*/
-define([], function () {
+define(['vagueTime.min'], function (vagueTime) {
     'use strict';
     var showTalk;
 
@@ -11,7 +11,8 @@ define([], function () {
 
     showTalk = function () {
         var ul = document.createElement('ul'),
-            talk = this.doc.talk;
+            talk = this.doc.talk,
+            now = new Date();
 
         talk.sort(function (a, b) {
             var dateA = new Date(a.timestamp),
@@ -19,8 +20,13 @@ define([], function () {
             return dateA - dateB;
         });
         talk.forEach(function (item) {
-            var li = document.createElement('li');
-            li.innerHTML = new Date(item.timestamp).toISOString() + ' - ' + item.text;
+            var li = document.createElement('li'),
+                vague = vagueTime({
+                    from: now,
+                    to: new Date(item.timestamp),
+                    lang: 'nl'
+                });
+            li.innerHTML = vague + ' - ' + item.text;
             ul.appendChild(li);
         });
         this.element.innerHTML = "";
