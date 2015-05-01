208179fa473bacc61589c0ebfaef53a7e3b80537
Pieter van der Eems <p.van.der.eems@interactiveblueprints.nl>
2015-05-01 15:45:54 +0200

    npcs, list all stats

--- js/npcs.js
+++ js/npcs.js
@@ -37,7 +37,7 @@ define(['pouchdb-3.4.0.min', 'skills'], function (Pouchdb, Skills) {
                 if (item.doc.gm) {
                     info = item.doc.gm.info || '';
                 }
-                li.innerHTML = item.doc.name + '(' + info +  ') <button type="button" "data-id="' + item.doc._id + '">&gt</button>';
+                li.innerHTML = item.doc.name + ' <span>(' + info +  ')</span> <button type="button" "data-id="' + item.doc._id + '">&gt</button>';
                 li.appendChild(div);
                 ul.appendChild(li);
             });
